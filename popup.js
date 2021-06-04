246692c397ea47ecc4983109c60867fdc81b0824
Parth Bhardwaj <parth@Parths-MacBook-Air.local>
2021-06-04 20:08:22 +0530

    implemented local storage for user notes

--- popup.js
+++ popup.js
@@ -1,81 +1,32 @@
-var noContentDiv = document.querySelector(".no-content");
-var addNoteBtn = document.querySelector(".add-btn");
-var inputDiv = document.querySelector(".inputdiv");
-var submitNoteBtn = document.querySelector(".plus-btn");
-var input = document.querySelector(".inputArea");
-var arrayOfNotes = [];
-
+const noContentDiv = document.querySelector(".no-content");
+const notesContainer = document.querySelector(".notes-container");
+const textBox = document.getElementById("notes");
 chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
-  var mylink = tabs[0].url;
-  addNoteBtn.addEventListener("click", () => {
-    addNoteBtn.style.display = "none";
-    inputDiv.style.display = "flex";
-  });
-  submitNoteBtn.addEventListener("click", () => {
-    var inputValue = input.value;
+  var currentUrl = tabs[0].url;
 
-    if (localStorage.getItem(mylink) === null) {
-      let arrayOfNotes = [];
-      storeNotes(mylink, arrayOfNotes, inputValue);
-      noContentDiv.style.display = "none";
-      displayNotes(inputValue);
-    } else {
-      let tempArr = JSON.parse(window.localStorage.getItem(mylink));
-      tempArr.push(inputValue);
-      window.localStorage.setItem(mylink, JSON.stringify(tempArr));
-      noContentDiv.style.display = "none";
-      displayNotes(inputValue);
-      clearInputArea();
-    }
-
-    //getNotes(mylink);
-  });
-  input.addEventListener("keydown", (e) => {
-    if (e.keyCode === 13) {
-      var inputValue = input.value;
-      if (localStorage.getItem(mylink) === null) {
-        arrayOfNotes = [];
-        storeNotes(mylink, arrayOfNotes, inputValue);
-        noContentDiv.style.display = "none";
-        displayNotes(inputValue);
-      } else {
-        let tempArr = JSON.parse(window.localStorage.getItem(mylink));
-        tempArr.push(inputValue);
-        window.localStorage.setItem(mylink, JSON.stringify(tempArr));
-        noContentDiv.style.display = "none";
-        displayNotes(inputValue);
-        clearInputArea();
+  if (localStorage.getItem(currentUrl) === null) {
+    window.addEventListener("keydown", (e) => {
+      if ((e.key = "Enter")) {
+        hideNoContentDiv();
+        notesContainer.style.display = "block";
+        textBox.focus();
       }
-    }
+    });
+  } else {
+    hideNoContentDiv();
+    notesContainer.style.display = "block";
+    let str = JSON.parse(window.localStorage.getItem(currentUrl));
+    textBox.value = str;
+    textBox.focus();
+  }
+  textBox.addEventListener("blur", () => {
+    var userNotes = textBox.value;
+    storeNotes(currentUrl, userNotes);
   });
 });
-
-function storeNotes(url, notes, inputVal) {
-  notes.push(inputVal);
-  input.value = "";
+function hideNoContentDiv() {
+  noContentDiv.style.display = "none";
+}
+function storeNotes(url, notes) {
   window.localStorage.setItem(url, JSON.stringify(notes));
-}
-/*
-function getNotes(url) {
-  var arr = JSON.parse(window.localStorage.getItem(url));
-  for (a = 0; a < arr.length; a++) {
-    var note = document.createElement("p");
-    note.innerText = arr[a];
-    note.className = "note";
-    var notesDiv = document.querySelector(".notes-div");
-    notesDiv.appendChild(note);
-  }
-}
-
-/*var getArray = window.localStorage.getItem(mylink);
-    console.log(getArray);*/
-function clearInputArea() {
-  input.value = "";
-}
-function displayNotes(val) {
-  let displayNote = document.createElement("p");
-  displayNote.className = "note";
-  displayNote.innerText = val;
-  var notesDiv = document.querySelector(".notes-div");
-  notesDiv.appendChild(displayNote);
 }
