94c36104b77781899c337e5f280b68b936b83cf2
Parth Bhardwaj <parth@Parths-MacBook-Air.local>
2021-06-08 12:48:02 +0530

    added backgrounf script to check if active tab has notes

---
+++ background.js
@@ -0,0 +1,18 @@
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  chrome.tabs.getSelected(null, function (tab) {
+    var url = tab.url;
+    var currentTabId = tab.tabId;
+    if (localStorage.getItem(url) !== null) {
+      chrome.browserAction.setBadgeBackgroundColor({ color: "green" }, () => {
+        chrome.browserAction.setBadgeText({ tabId: currentTabId, text: "*" });
+      });
+    } else {
+      chrome.browserAction.setBadgeBackgroundColor({ color: "green" }, () => {
+        chrome.browserAction.setBadgeText({
+          tabId: currentTabId,
+          text: "",
+        });
+      });
+    }
+  });
+});
