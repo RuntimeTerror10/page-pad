6fd856cef8676cb3125d4eac08bb6067f4ef52ca
Parth Bhardwaj <parthb.git@gmail.com>
2021-06-23 01:13:00 +0530

    categorize notes of webpages with their respective websites and display them

---
+++ main.js
@@ -0,0 +1,44 @@
+const container = document.querySelector(".all-notes-container");
+
+var urlKeys = [];
+for (let i = 0; i < localStorage.length; i++) {
+  let key = localStorage.key(i);
+  urlKeys.push(key);
+}
+
+var urlMap = new Map();
+
+for (let x = 0; x < urlKeys.length; x++) {
+  var tempUrl = new URL(urlKeys[x]);
+  var key = tempUrl.hostname;
+
+  if (urlMap.has(key)) {
+    var fetchedArr = urlMap.get(key);
+    fetchedArr.push(urlKeys[x]);
+    urlMap.set(key, fetchedArr);
+  } else {
+    var arr = [];
+    arr.push(urlKeys[x]);
+    urlMap.set(key, arr);
+  }
+}
+
+urlMap.forEach(function (value, key) {
+  const subContainer = document.createElement("div");
+  subContainer.innerHTML = `<h1>${key}</h1>`;
+  subContainer.className = "site-notes";
+  for (let i = 0; i < value.length; i++) {
+    const userObj = JSON.parse(localStorage.getItem(value[i]));
+    var tabTitle = userObj.title;
+    var tabNote = userObj.notes;
+    const noteTab = document.createElement("details");
+
+    noteTab.className = "all-notes-tab";
+    noteTab.innerHTML = `
+            <summary class="summary-heading">${tabTitle}</summary>
+            <textarea rows="6" cols="30" spellcheck="false" readonly class="readonly-textarea notepad" >${tabNote}</textarea>
+          `;
+    subContainer.appendChild(noteTab);
+  }
+  container.appendChild(subContainer);
+});
