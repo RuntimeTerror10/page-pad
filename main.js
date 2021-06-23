2145795d88d40fded7be4fdeb19e0d457de00957
Parth Bhardwaj <parthb.git@gmail.com>
2021-06-23 12:14:25 +0530

    add page hrefs for each note

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ for (let x = 0; x < urlKeys.length; x++) {
 
 urlMap.forEach(function (value, key) {
   const subContainer = document.createElement("div");
-  subContainer.innerHTML = `<h1>${key}</h1>`;
+  subContainer.innerHTML = `<h1 style ="color:#333">${key}</h1>`;
   subContainer.className = "site-notes";
   for (let i = 0; i < value.length; i++) {
     const userObj = JSON.parse(localStorage.getItem(value[i]));
@@ -37,6 +37,7 @@ urlMap.forEach(function (value, key) {
     noteTab.innerHTML = `
             <summary class="summary-heading">${tabTitle}</summary>
             <textarea rows="6" cols="30" spellcheck="false" readonly class="readonly-textarea notepad" >${tabNote}</textarea>
+            <div class="link-wrap"><a class="visit-link" target="_blank" href=${value[i]}>VISIT THIS PAGE</a></div>
           `;
     subContainer.appendChild(noteTab);
   }
