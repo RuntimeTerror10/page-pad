a65a535db083eccfc2c597e11dd2a34af1d46e5e
Parth Bhardwaj <parthb.git@gmail.com>
2021-07-10 15:35:09 +0530

    style changes for text editor and menu burger

---
+++ app.js
@@ -0,0 +1,12 @@
+const menuBtn = document.querySelector(".menu-btn");
+const burger = document.querySelector(".menu-btn-burger");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
+});
