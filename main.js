f980f7467d002accbaa9c38e4426f6d72e68922f
Parth Bhardwaj <parthb.git@gmail.com>
2021-07-10 15:35:41 +0530

    added notes filtering feature

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const container = document.querySelector(".all-notes-container");
+const searchBar = document.querySelector(".search-input");
+const filterContainer = document.querySelector(".filter-notes-container");
 
 var urlKeys = [];
 for (let i = 0; i < localStorage.length; i++) {
@@ -36,10 +38,68 @@ urlMap.forEach(function (value, key) {
     noteTab.className = "all-notes-tab";
     noteTab.innerHTML = `
             <summary class="summary-heading">${tabTitle}</summary>
-            <textarea rows="6" cols="30" spellcheck="false" readonly class="readonly-textarea notepad" >${tabNote}</textarea>
+            <div class="readonly">${tabNote}</div>
             <div class="link-wrap"><a class="visit-link" target="_blank" href=${value[i]}>VISIT THIS PAGE</a></div>
           `;
     subContainer.appendChild(noteTab);
   }
   container.appendChild(subContainer);
 });
+
+/* notes searching*/
+searchBar.addEventListener("keyup", () => {
+  if (searchBar.value.length === 0) {
+    showAllNotes();
+    clearResult();
+  }
+  if (searchBar.value.length > 0) {
+    hideAllNotes();
+    var userInput = searchBar.value.toLowerCase();
+    clearResult();
+
+    for (let k = 0; k < urlKeys.length; k++) {
+      var fetchedObj = JSON.parse(localStorage.getItem(urlKeys[k]));
+      var displayTitle = fetchedObj.title;
+      var objTitle = fetchedObj.title.toLowerCase();
+
+      var compareResult = isSubstring(userInput, objTitle);
+      if (compareResult == -1) {
+        const e = 0;
+      } else {
+        var filterNote = fetchedObj.notes;
+        const noteTab = document.createElement("details");
+
+        noteTab.className = "all-notes-tab";
+        noteTab.innerHTML = `
+            <summary class="summary-heading">${displayTitle}</summary>
+            <div class="readonly">${filterNote}</div>`;
+
+        filterContainer.appendChild(noteTab);
+      }
+    }
+  }
+});
+
+function showAllNotes() {
+  container.style.display = "block";
+}
+
+function hideAllNotes() {
+  container.style.display = "none";
+}
+function clearResult() {
+  filterContainer.innerHTML = "";
+}
+function isSubstring(s1, s2) {
+  var query = s1.length;
+  var string = s2.length;
+
+  for (var i = 0; i <= string - query; i++) {
+    var j;
+    for (j = 0; j < query; j++) if (s2[i + j] != s1[j]) break;
+
+    if (j == query) return i;
+  }
+
+  return -1;
+}
